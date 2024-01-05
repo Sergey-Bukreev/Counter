44c88d133053276360c8d0c73a3ee228fa3123e5
BukreevSergey <bukreevwork2@gmail.com>
2024-01-05 15:27:42 +0200

    create setting block and use local storage for save settings and values. add more styles for counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,68 @@
 
-import React, { useState } from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import './App.css';
 import { Button } from "./components/myButton/Button";
 
 function App() {
-    let [num, setNum] = useState(0);
 
-    const addNum = () => {num < 5? setNum(++num):setNum(num)};
+    let [minNum, setMinNum] = useState(0)
+    let [maxNum, setMaxNum] = useState(5)
+    let [num, setNum] = useState(minNum);
 
-    const resetNum = () => {setNum(0)};
+    useEffect(() => {
+        let valueCounter=localStorage.getItem("value")
+       if(valueCounter) {setNum(JSON.parse(valueCounter))}
+    }, []);
 
-    const numberClass = num === 5 ? 'maxCount' : '';
-    const isResetDisabled = num === 0;
-    const isAddNumDisabled = num === 5;
+    useEffect(() => {
+        localStorage.setItem("value", JSON.stringify(num))
+    }, [num]);
+    useEffect(() => {
+       let minValueCounter =localStorage.getItem("minValue");
+       if(minValueCounter) {setMinNum(JSON.parse(minValueCounter))}
+       }, []);
+    useEffect(() => {
+        localStorage.setItem("minValue", JSON.stringify(minNum))
+    }, [minNum]);
+
+    useEffect(() => {
+        let maxValueCounter =localStorage.getItem("maxValue");
+        if(maxValueCounter) {setMaxNum(JSON.parse(maxValueCounter))}
+    }, []);
+    useEffect(() => {
+        localStorage.setItem("maxValue", JSON.stringify(maxNum))
+    }, [maxNum]);
+    const addNum = () => {
+        num < maxNum? setNum(++num):setNum(num); };
+
+    const resetNum = () => {setNum(minNum); setMaxNum(maxNum)};
+    const addSettings = ()=> {setNum(minNum)}
+    const onChangeMaxNum = (e:ChangeEvent<HTMLInputElement>)=> {setMaxNum(Number(e.currentTarget.value))}
+    const onChangeMinNum = (e:ChangeEvent<HTMLInputElement>)=> {setMinNum(Number(e.currentTarget.value))}
+
+    const numberClass = num === maxNum ? 'maxCount' : '';
+    const isResetDisabled = num === minNum;
+    const isAddNumDisabled = num === maxNum;
 
     return (
         <div className="App">
+            <div className={"contentWrapper"}>
+                <div className={"settingWrapper" }>
+                   <div className={"itemSetWrapper"}>
+                       <h3>Max Value</h3>
+                       <input type={"number"} value={maxNum} onChange={onChangeMaxNum}/>
+                   </div>
+                    <div className={"itemSetWrapper"}>
+                        <h3>Min Value</h3>
+                        <input type={"number"} value={minNum} onChange={onChangeMinNum}/>
+                    </div>
+                </div>
+                <div className={"buttonWrapper"}>
+                    <Button title={"SET"} callBack={addSettings} disabled={isAddNumDisabled} />
+                </div>
+            </div>
+
+
             <div className={"contentWrapper"}>
                 <div className={`numberWrapper ${numberClass}`}>
                     <h1>{num}</h1>
